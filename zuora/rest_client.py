4b040a2780eece35a55887fec15e65268e0b6e66
Naoya Kanai <naopon@gmail.com>
2015-07-01 14:32:24 -0700

    disable InsecureRequestWarning on REST requests

--- zuora/rest_client.py
+++ zuora/rest_client.py
@@ -1,10 +1,9 @@
 import requests
-import urllib3
 from rest_wrapper import (AccountManager, CatalogManager, PaymentMethodManager,
                           SubscriptionManager, TransactionManager,
                           UsageManager)
 
-urllib3.disable_warnings()
+requests.packages.urllib3.disable_warnings()
 ## This file contains some parameters that will need to be changed to work in different tenants:
 ## REQUIRED PARAMS:
 ## username: change to an API user that has REST write capabilities
